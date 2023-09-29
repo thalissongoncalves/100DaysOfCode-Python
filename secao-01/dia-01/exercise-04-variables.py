343e9f5003acf6cd9017f3fbd023873ea935b0e8
thalissongoncalves <thalissongsilva70@gmail.com>
2023-09-29 10:46:20 -0300

    Feito exercício 04 - Seção 01 - Dia 01

---
+++ secao-01/dia-01/exercise-04-variables.py
@@ -0,0 +1,20 @@
+# Aplica um pouco as regras de variáveis para inverter os valores de a e b
+
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+c = a # 6
+a = b # 9
+b = c # 6
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
