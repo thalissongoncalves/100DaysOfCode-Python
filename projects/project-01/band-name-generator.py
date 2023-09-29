362b3d2fc4170129c3fe344f57f62acf79b4583e
thalissongoncalves <thalissongsilva70@gmail.com>
2023-09-29 14:29:10 -0300

    Refatora código do Projeto 01 e adiciona versão para português

--- projects/project-01/band-name-generator.py
+++ projects/project-01/band-name-generator.py
@@ -1,8 +1,4 @@
-# Band Name Generator
 print("Welcome to the Band Name Generator")
-print("What's the name of the city you grew up in?")
-city = input()
-print("What's your pet's name?")
-pet = input()
-band_name = city + " " + pet
-print("Your band name could be " + band_name)
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be: " + city + " " + pet)
---
+++ projects/project-01/gerador-de-nome-de-banda.py
@@ -0,0 +1,4 @@
+print("Seja bem-vindo ao Gerador de Nome de Bandas")
+cidade = input("Qual o nome da cidade em que você cresceu?\n")
+animal = input("Qual o nome de seu animal de estimação?\n")
+print("Nome da sua banda seria: " + cidade + " " + animal)
