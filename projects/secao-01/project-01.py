188f7c49504e75b58f06dd980f366ebfce3a855b
thalissongoncalves <thalissongsilva70@gmail.com>
2023-09-29 11:20:46 -0300

    Feito Projeto 01 - Seção 01

---
+++ projects/secao-01/project-01.py
@@ -0,0 +1,8 @@
+# Band Name Generator
+print("Welcome to the Band Name Generator")
+print("What's the name of the city you grew up in?")
+city = input()
+print("What's your pet's name?")
+pet = input()
+band_name = city + " " + pet
+print("Your band name could be " + band_name)
