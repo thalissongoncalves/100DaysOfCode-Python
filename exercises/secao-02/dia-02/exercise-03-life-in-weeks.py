0bfb93b06792da2ad71921fa880631f8f673e8aa
thalissongoncalves <thalissongsilva70@gmail.com>
2023-10-02 15:55:23 -0300

    Feito exercício 03 - Seção 02 - Dia 03

---
+++ exercises/secao-02/dia-02/exercise-03-life-in-weeks.py
@@ -0,0 +1,11 @@
+age = input("What is your current age? ")
+
+ninety_month = 1080
+ninety_week = 4680
+ninety_day = 32850
+
+age_month = int(age) * 12
+age_week = int(age) * 52
+age_day = int(age) * 365
+
+print(f"You have {ninety_day - age_day} days, { ninety_week - age_week } weeks, and { ninety_month - age_month } months left.")
