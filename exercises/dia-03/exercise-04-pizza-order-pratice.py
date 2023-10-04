bcb0ae6727d0be4fba9d398168f233a9938c3033
thalissongoncalves <thalissongsilva70@gmail.com>
2023-10-04 12:00:09 -0300

    Reajuste exercicio 04 - dia 03

--- exercises/dia-03/exercise-04-pizza-order-pratice.py
+++ exercises/dia-03/exercise-04-pizza-order-pratice.py
@@ -5,23 +5,22 @@ extra_cheese = input() # Do you want extra cheese? Y or N
 # 🚨 Don't change the code above 👆
 # Write your code below this line 👇
 
-# My code:
-
 total_value = 0
+
 if size == "S":
-  total_value += 15
+    total_value += 15
 elif size == "M":
-  total_value += 20
+    total_value += 20
 else:
-  total_value += 25
+    total_value += 25
 
 if add_pepperoni == "Y":
-  if size == "S":
-    total_value += 2
-  else:
-    total_value += 3
+    if size == "S":
+        total_value += 2
+    else:
+        total_value += 3
 
 if extra_cheese == "Y":
-  total_value += 1
+    total_value += 1
 
-print(f"Your final total_value is: ${total_value}.")
+print(f"Your final bill is: ${total_value}.")
