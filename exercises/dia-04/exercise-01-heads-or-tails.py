1f6f01e17db561b476bdc16142ec5a9fafd46ffa
thalissongoncalves <thalissongsilva70@gmail.com>
2023-10-05 11:06:50 -0300

    Feito exercicio 01 - dia 04

---
+++ exercises/dia-04/exercise-01-heads-or-tails.py
@@ -0,0 +1,8 @@
+# Write your code below this line 👇
+# Hint: Remember to import the random module first. 🎲
+import random
+random_option = random.randint(0, 1)
+if random_option == 1:
+  print("Heads")
+if random_option == 0:
+  print("Tails")
