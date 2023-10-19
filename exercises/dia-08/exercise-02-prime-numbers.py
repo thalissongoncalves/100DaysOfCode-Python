4e57e83455533bf33ff71611b21aa3a3ef6fef18
thalissongoncalves <thalissongsilva70@gmail.com>
2023-10-19 17:46:32 -0300

    Feito exercício 02 - dia 08

---
+++ exercises/dia-08/exercise-02-prime-numbers.py
@@ -0,0 +1,18 @@
+# Write your code below this line 👇
+
+def prime_checker(number):
+  checkPrime = True
+  for num in range(2, number):
+    if number % num == 0:
+      checkPrime = False
+  if checkPrime:
+    print("It's a prime number.")
+  else:
+    print("It's not a prime number.")
+
+
+# Write your code above this line 👆
+    
+#Do NOT change any of the code below👇
+n = int(input()) # Check this number
+prime_checker(number=n)
