76a61748890705a27e81a0581b2d64811270d0b1
thalissongoncalves <thalissongsilva70@gmail.com>
2023-10-20 09:30:51 -0300

    Feito exercício 04 - dia 08

---
+++ exercises/dia-08/exercise-04-decrypt.py
@@ -0,0 +1,31 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def encrypt(input_text, input_shift):
+  word = ""
+  for letter in input_text:
+    index = alphabet.index(f"{letter}")
+    word += alphabet[index + input_shift]
+  print(f"The encoded text is {word}")
+
+#TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
+def decrypt(input_text, input_shift):
+  word = ""
+  for letter in input_text:
+    index = alphabet.index(f"{letter}")
+    word += alphabet[index - input_shift]
+  print(f"The decoded text is {word}")
+  #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.  
+  #e.g. 
+cipher_text = "mjqqt"
+  #shift = 5
+  #plain_text = "hello"
+  #print output: "The decoded text is hello"
+
+
+#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message. 
+
+decrypt(input_text=cipher_text, input_shift=shift)
