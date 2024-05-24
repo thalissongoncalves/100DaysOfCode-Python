8b799372db8d2a206dc5e627e080935bbeaaeaa3
thalissongoncalves <thalissongsilva70@gmail.com>
2024-05-24 16:40:06 -0300

    .

---
+++ exercises/day-15/data.py
@@ -0,0 +1,10 @@
+contacts = {
+    "Thalisson": {
+        "phone_number": 35991144185,
+        "email": "thalissongsilva70@gmail.com",
+    },
+    "Jotinha": {
+        "phone_number": 35999999999,
+        "email": "thalissongdev@gmail.com",
+    },
+}
---
+++ exercises/day-15/main.py
@@ -0,0 +1,5 @@
+from data import contacts
+
+thalisson_email = contacts["Thalisson"]["email"]
+
+print(thalisson_email)
